e0ae4dcbcf9d6c21ca3fdfa96ecf8675ec0a26ac
Arunav Sen <arunavsen96@gmail.com>
2022-11-13 22:38:41 +0600

    Create Values Controller

---
+++ Dating Wep Api/Controllers/ValuesController.cs
@@ -0,0 +1,39 @@
+﻿using Dating_Wep_Api.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dating_Wep_Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly ApplicationDBContext _context;
+
+        public ValuesController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet(Name = "ListOfValues")]
+        public IActionResult GetValues()
+        {
+            return Ok(_context.Values.ToList());
+        }
+
+        [HttpGet("{id}",Name ="OnlySingleValue")]
+        public IActionResult GetValue(int id)
+        {
+            if (id is not 0)
+            {
+                var value = _context.Values.FirstOrDefault(x => x.Id == id);
+
+                if (value == null) 
+                { 
+                    return BadRequest(); 
+                }
+                return Ok(value);
+            }
+            return BadRequest();
+        }
+    }
+}
