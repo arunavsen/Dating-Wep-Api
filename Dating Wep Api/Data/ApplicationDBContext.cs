bb621db59220e30fd254d9fb529b09aa73894861
Arunav Sen <arunavsen96@gmail.com>
2022-11-13 21:38:24 +0600

    Add Value Class, Add DbContext Class, Add Connection String, Add Use sql Server in the sartup class

---
+++ Dating Wep Api/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Dating_Wep_Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dating_Wep_Api.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
