f8273d29975bf4d41f72f1223cfe62fe9900306c
Arunav Sen <arunavsen96@gmail.com>
2022-11-26 11:01:20 +0600

    Create IAuthRepository

--- Dating Wep Api/Data/ApplicationDBContext.cs
+++ Dating Wep Api/Data/ApplicationDBContext.cs
@@ -11,5 +11,6 @@ namespace Dating_Wep_Api.Data
         }
 
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
---
+++ Dating Wep Api/Data/IRepository/IAuthRepository.cs
@@ -0,0 +1,11 @@
+﻿using Dating_Wep_Api.Models;
+
+namespace Dating_Wep_Api.Data.IRepository
+{
+    public interface IAuthRepository
+    {
+        Task<User> RegisterUser(User user, string password);
+        Task<User> Login(string username, string password);
+        Task<bool> UserExist(string username);
+    }
+}
