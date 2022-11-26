8bebc45448776367889ed242d37730382252486c
Arunav Sen <arunavsen96@gmail.com>
2022-11-26 18:23:02 +0600

    Create Login method and JWT token

--- Dating Wep Api/Controllers/AuthController.cs
+++ Dating Wep Api/Controllers/AuthController.cs
@@ -2,6 +2,10 @@
 using Dating_Wep_Api.DTO;
 using Dating_Wep_Api.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace Dating_Wep_Api.Controllers
 {
@@ -10,10 +14,12 @@ namespace Dating_Wep_Api.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repo;
+        private readonly IConfiguration _config;
 
-        public AuthController(IAuthRepository repo)
+        public AuthController(IAuthRepository repo, IConfiguration config)
         {
             _repo = repo;
+            _config = config;
         }
 
         [HttpPost("register")]
@@ -36,6 +42,44 @@ namespace Dating_Wep_Api.Controllers
             var createdUser = await _repo.RegisterUser(userToCreate, userForRegisterDTO.password);
 
             return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDTO userForLoginDTO)
+        {
+            var userFromRepo = await _repo.Login(userForLoginDTO.Username, userForLoginDTO.Password);
+
+            if (userFromRepo == null)
+            {
+                return Unauthorized();
+            }
+
+            #region How to create token after login
+            
+            var claims = new[] {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
+            };
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("Appsettings:Token").Value));
+
+            var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor {
+                        Subject = new ClaimsIdentity(claims),
+                        Expires = DateTime.Now.AddMinutes(120),
+                        SigningCredentials = signingCredentials
+                        };
+
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+            #endregion
+
+            return Ok(tokenString);
         }
 
     }
---
+++ Dating Wep Api/DTO/UserForLoginDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Dating_Wep_Api.DTO
+{
+    public class UserForLoginDTO
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
