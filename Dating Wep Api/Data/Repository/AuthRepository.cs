bcdae7cc32218af6925bedbf15dda1168f2bb4b0
Arunav Sen <arunavsen96@gmail.com>
2022-11-26 11:32:54 +0600

    Create Register method

---
+++ Dating Wep Api/Data/Repository/AuthRepository.cs
@@ -0,0 +1,49 @@
+﻿using Dating_Wep_Api.Data.IRepository;
+using Dating_Wep_Api.Models;
+
+namespace Dating_Wep_Api.Data.Repository
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly ApplicationDBContext _context;
+
+        public AuthRepository(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        public Task<User> Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<User> RegisterUser(User user, string password)
+        {
+            byte[] passwordHash, passwordSalt;
+
+            CreatePasswordHash(password, out passwordHash, out passwordSalt);
+
+            user.PasswordSalt = passwordSalt;
+            user.PasswordHash = passwordHash;
+
+            await _context.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public Task<bool> UserExist(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
