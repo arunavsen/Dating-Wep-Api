2beee5913697a7a68ae3939689050d3a8d806a2e
arunav sen <arunavsen96@gmail.com>
2023-04-29 20:00:22 +0600

    Entity Framework relationships

---
+++ Dating Wep Api/Migrations/20230429133834_UpgradePhotoClass.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DatingWepApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpgradePhotoClass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Photos_Users_UserId",
+                table: "Photos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Photos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Photos_Users_UserId",
+                table: "Photos",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Photos_Users_UserId",
+                table: "Photos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Photos",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Photos_Users_UserId",
+                table: "Photos",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
