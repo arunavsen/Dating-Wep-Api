c00712bc33e169a66b3fa6cf4d14a9967a1cd12b
arunav sen <arunavsen96@gmail.com>
2023-05-19 02:39:23 +0600

    Add User List and Details DTO

---
+++ Dating Wep Api/DTO/UserForDetails.cs
@@ -0,0 +1,22 @@
+﻿using Dating_Wep_Api.Models;
+
+namespace Dating_Wep_Api.DTO
+{
+    public class UserForDetails
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastActivatedAt { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interestes { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PhotoUrl { get; set; }
+        public ICollection<Photo> Photos { get; set; }
+    }
+}
---
+++ Dating Wep Api/DTO/UserForListDTO.cs
@@ -0,0 +1,18 @@
+﻿using Dating_Wep_Api.Models;
+
+namespace Dating_Wep_Api.DTO
+{
+    public class UserForListDTO
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastActivatedAt { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PhotoUrl { get; set; }
+    }
+}
