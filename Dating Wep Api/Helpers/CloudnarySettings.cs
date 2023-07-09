39a26511664320ba163b9f9dbc9721e3009d7d95
arunav sen <arunavsen96@gmail.com>
2023-07-09 23:50:45 +0600

    Using Cloudinary as storage of Photos

---
+++ Dating Wep Api/Helpers/CloudnarySettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Dating_Wep_Api.Helpers
+{
+    public class CloudnarySettings
+    {
+        public string CloudName { get; set; }
+        public string ApiKey { get; set; }
+        public string ApiSecret { get; set; }
+    }
+}
